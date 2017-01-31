f5a781e13ea16428bcf3da1812413f5baa0d2ba8
benshanahan1 <benshanahan1@gmail.com>
2017-01-31 17:00:37 -0500

    fixed typo

--- application/views/pages/about-me.php
+++ application/views/pages/about-me.php
@@ -8,7 +8,7 @@
 
                     <div class="col-md-9">
                         <h2>About me</h2>
-                        <p>I am currently a senior at Brown University in Rhode Island, where I study Neuroscience and French, among other things. In the occasional free moment, I love to code, draw, make music, and go running, rock climbing, and bicycling. My recent projects have revolved around exploring the Internet of Things and integrating the physical world with the web.</p>
+                        <p>I am currently a senior at Brown University in Rhode Island, where I study Neuroscience and French, among other things. In the occasional free moment, I love to code, draw, make music, and go running, rock climbing, and bicycling. My recent projects revolve around the Internet of Things and the integration of the physical world with the web.</p>
                         <p>I am currently researching and writing an undergraduate thesis with the <a href="http://www.braingate.org" target="_blank">BrainGate</a> lab at Brown, exploring different techniques for differentiating behavioral state based on brain activity.</p>
                         <p>You can find my current <a href="documents/ShanahanBenjaminCV.pdf" target="_blank">CV</a> here.</p>
                     </div>
